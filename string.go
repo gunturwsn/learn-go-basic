1b4fbf34a9dca703d09dabd4a32ae063f4d53e2e
gunturwsn <gunturw79@gmail.com>
2024-01-07 22:41:58 +0700

    Day 1 learn go

---
+++ string.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Jakarta")
+	fmt.Println("South Jakarta")
+
+	/* Function for string */
+	fmt.Println(len("Jakarta"))
+	fmt.Println("Jakarta"[1])
+}
